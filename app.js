108bae9b890907d72c3ff80402b8bee8447dbe42
Kirill Serebrykov <kirill.baza22@gmail.com>
2022-07-19 14:24:50 +0300

    fix server.listen

--- app.js
+++ app.js
@@ -54,4 +54,9 @@ if (cluster.isMaster) {
 }
 */
 
-await fastify.listen(port, host || '0.0.0.0');
+fastify.listen({port : process.env.PORT || 3000}, (err) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+})
