23e6af40e98fe11d775efac7105e6908bc5747e8
Kirill Serebrykov <kirill.baza22@gmail.com>
2022-07-20 14:10:32 +0300

    test with parser

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
 import Fastify from 'fastify'
 import {GetPage} from './puppeteer.js'
 import cluster  from "cluster";
-//import os from "os"
-//const numCPUs = os.cpus().length
+import os from "os"
+const numCPUs = os.cpus().length
 
-//let price = 0
+let price = 0
 const fastify = Fastify({
   logger: true,
 })
 
-const schema = {
-  schema: {
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          hello: {
-            type: 'string'
-          }
-        }
-      }
-    }
-  }
-}
-
-fastify.get('/', schema, function (req, reply) {
-    reply.send({ hello: 'world' })
-  })
-
-fastify.listen(process.env.PORT, '0.0.0.0', err => {
-  if (err) throw err
-  console.log(`server listening on ${fastify.server.address().port}`)
-})
 
 
 
-/*
 
 fastify.get('/', async (request, reply) => {
     reply.header("Access-Control-Allow-Origin", "*");
@@ -48,8 +24,8 @@ fastify.get('/', async (request, reply) => {
 fastify.get('/getDataComponent', async (request, reply) => {
         reply.header("Access-Control-Allow-Origin", "*");
         reply.header("Access-Control-Allow-Methods", "GET");
-       // const url_site = new URL(request.query.url)
-       //return GetPage(url_site).then(value => value)
+        const url_site = new URL(request.query.url)
+        return GetPage(url_site).then(value => value)
     })
 
 
@@ -62,13 +38,8 @@ if (cluster.isMaster) {
         console.log(`Worker ${worker.process.pid} died`);
     });
 } else {
-    
-    fastify.listen({
-        port,
-		host,
-        maxRequestsPerSocket: 199
-    }, () => {
-        console.log(`Fastify "start" listening on port ${port}, PID: ${process.pid}`);
-    });
+  fastify.listen(process.env.PORT, '0.0.0.0', err => {
+    if (err) throw err
+    console.log(`server listening on ${fastify.server.address().port}`)
+  })
 }
-*/
